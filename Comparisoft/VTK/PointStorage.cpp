4f297fe69698c34d95ef12b52290390078194966
Karn <karnseaman@gmail.com>
2018-01-27 23:01:34 -0800

    Started work on data storage class.

---
+++ Comparisoft/VTK/PointStorage.cpp
@@ -0,0 +1,15 @@
+#include "PointStorage.h"
+using namespace std;
+
+class pointStorage {
+
+private: class point {
+	double pos[3];
+	double nearestPos[3];
+	double distance;
+};
+
+	private vector<point> points;
+
+
+};
--- Comparisoft/VTK/VTK.h
+++ Comparisoft/VTK/VTK.h
@@ -2,5 +2,5 @@
 
 #include <string>
 
-std::string utf8_encode(const std::wstring &wstr);
+//std::string utf8_encode(const std::wstring &wstr);
 int VTKmain(char* filePathReferance, char* filePathProduction);
