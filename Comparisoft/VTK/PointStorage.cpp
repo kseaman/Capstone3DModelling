3234f45c59fe4ef206db9979726610e3edebe98e
Karn <karnseaman@gmail.com>
2018-01-29 00:53:20 -0800

    Worked on storage class

--- Comparisoft/VTK/PointStorage.cpp
+++ Comparisoft/VTK/PointStorage.cpp
@@ -1,15 +1,56 @@
 #include "PointStorage.h"
 using namespace std;
 
-class pointStorage {
+class pointStore {
 
-private: class point {
-	double pos[3];
-	double nearestPos[3];
-	double distance;
-};
+//store all of the points and the information about them
+private: 
+	class point {
+		double pos[3];
+		double nearestPos[3];
+		double distance;
 
-	private vector<point> points;
+	public:
+		//class constructor
+		point(double pointpos[3], double near[3], double dist) {
+			memcpy(pos, pointpos, sizeof(pos));
+			memcpy(nearestPos, near, sizeof(nearestPos));
+			distance = dist;
+		}
 
+		//return the point position
+		double* getPos() {
+			return pos;
+		}
 
+		//return nearest position
+		double* getNearestPos() {
+			return nearestPos;
+		}
+
+		//return distance
+		double getDistance() {
+			return distance;
+		}
+	};
+
+	//set member variables
+	int currpos;
+	int size;
+	vector<point> points;
+
+public:
+
+	//constructor
+	pointStore(int size) {
+		currpos = 0;
+		this->size = size;
+		points.reserve(size);
+	}
+
+	//add a new point to the vector
+	void add(double pointpos[3], double near[3], double dist) {
+		points.push_back(point(pointpos, near, dist));
+		currpos++;
+	}
 };
--- Comparisoft/VTK/PointStorage.h
+++ Comparisoft/VTK/PointStorage.h
@@ -1,2 +1,26 @@
 #pragma once
 #include <vector>
+
+class pointStore {
+
+private: 
+	class point {
+		double pos[3];
+		double nearestPos[3];
+		double distance;
+
+	public:
+		point(double pointpos[3], double near[3], double dist);
+		double* getPos();
+		double* getNearestPos();
+		double getDistance();
+	};
+
+	int currpos;
+	int size;
+	vector<point> points;
+
+public:
+	pointStore(int size);
+	void add(double pointpos[3], double near[3], double dist);
+};
