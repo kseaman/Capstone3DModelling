0b8da372ae2e3aa3b15d983dcf6383c431810501
danaack <danak@shaw.ca>
2018-02-06 12:16:41 -0800

    Create HTML comparison report for alignment output
    Report is saved in the save location directory passed in from Qt.

--- Comparisoft/VTK/VSVTK.cxx
+++ Comparisoft/VTK/VSVTK.cxx
@@ -11,8 +11,10 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <string>
+#include <fstream>
 #include "VTK.h"
 #include "fileDialogue.h"
+using namespace std;
 
 
 
@@ -56,118 +58,143 @@ int main(int argc, char *argv[])
 
 	//argument 1: Client_Name
 	cname = argv[1];
-	std::cout << "1: cname\n";
-	std::cout << cname;
-	std::cout << "\n";
+	cout << "1: cname\n";
+	cout << cname;
+	cout << "\n";
 
 	//argument 2: Patient_Name
 	pname = argv[2];
-	std::cout << "2: pname\n";
-	std::cout << pname;
-	std::cout << "\n";
+	cout << "2: pname\n";
+	cout << pname;
+	cout << "\n";
 
 	//argument 3: File_Description
 	fdesc = argv[3];
-	std::cout << "3: fdesc\n";
-	std::cout << fdesc;
-	std::cout << "\n";
+	cout << "3: fdesc\n";
+	cout << fdesc;
+	cout << "\n";
 
 	//argument 4: Save_Location
 	sloc = argv[4];
-	std::cout << "4: sloc\n";
-	std::cout << sloc;
-	std::cout << "\n";
+	cout << "4: sloc\n";
+	cout << sloc;
+	cout << "\n";
 
 	//argument 5: File_Name
 	fname = argv[5];
-	std::cout << "5: fname\n";
-	std::cout << fname;
-	std::cout << "\n";
+	cout << "5: fname\n";
+	cout << fname;
+	cout << "\n";
 
 	//argument 6: Technician_Name
 	tname = argv[6];
-	std::cout << "6: tname\n";
-	std::cout << tname;
-	std::cout << "\n";
+	cout << "6: tname\n";
+	cout << tname;
+	cout << "\n";
 
 	//argument 7: Product_Name
 	prodname = argv[7];
-	std::cout << "7: pname\n";
-	std::cout << pname;
-	std::cout << "\n";
+	cout << "7: pname\n";
+	cout << pname;
+	cout << "\n";
 
 	//argument 8: Production_Date
 	pdate = argv[8];
-	std::cout << "8: pdate\n";
-	std::cout << pdate;
-	std::cout << "\n";
+	cout << "8: pdate\n";
+	cout << pdate;
+	cout << "\n";
 
 	//argument 9: Product_Description
 	pdesc = argv[9];
-	std::cout << "9: pdesc\n";
-	std::cout << pdesc;
-	std::cout << "\n";
+	cout << "9: pdesc\n";
+	cout << pdesc;
+	cout << "\n";
 
 	//argument 10: Report_Type
 	rtype = argv[10];
-	std::cout << "10: rtype\n";
-	std::cout << rtype;
-	std::cout << "\n";
+	cout << "10: rtype\n";
+	cout << rtype;
+	cout << "\n";
 
 	//argument 11: Confidence_Level
 	clevel = argv[11];
-	std::cout << "11: clevel\n";
-	std::cout << clevel;
-	std::cout << "\n";
+	cout << "11: clevel\n";
+	cout << clevel;
+	cout << "\n";
 
 	//argument 12: Error_Bound
 	ebound = argv[12];
-	std::cout << "12: ebound\n";
-	std::cout << ebound;
-	std::cout << "\n";
+	cout << "12: ebound\n";
+	cout << ebound;
+	cout << "\n";
 
 	//argument 13: Error_Unit
 	eunit = argv[13];
-	std::cout << "13: eunit\n";
-	std::cout << eunit;
-	std::cout << "\n";
+	cout << "13: eunit\n";
+	cout << eunit;
+	cout << "\n";
 
 	//argument 14: Alignment_Type
 	atype = argv[14];
-	std::cout << "14: atype\n";
-	std::cout << atype;
-	std::cout << "\n";
+	cout << "14: atype\n";
+	cout << atype;
+	cout << "\n";
 
 	//argument 15: Ref_Attempt
 	refattempt = argv[15];
-	std::cout << "15: refattempt\n";
-	std::cout << refattempt;
-	std::cout << "\n";
+	cout << "15: refattempt\n";
+	cout << refattempt;
+	cout << "\n";
 
 	//argument 16: Prod_Attempt
 	prodattempt = argv[16];
-	std::cout << "16: prodattempt\n";
-	std::cout << prodattempt;
-	std::cout << "\n";
+	cout << "16: prodattempt\n";
+	cout << prodattempt;
+	cout << "\n";
 
 	//argument 17: referance file
 	filePathReference = argv[17];
-	std::cout << "17: ref file\n";
-	std::cout << filePathReference;
-	std::cout << "\n";
+	cout << "17: ref file\n";
+	cout << filePathReference;
+	cout << "\n";
 
 	//argument 18+:production file(s)
 	filePathProduction = argv[18];
-	std::cout << "18: production file\n";
-	std::cout << filePathProduction;
-	std::cout << "\n";
-	
-
-	
+	cout << "18: production file\n";
+	cout << filePathProduction;
+	cout << "\n";
 
 	//Hardcoded files for testing
 	//filePathReference = "C:/Development/Capstone/Capstone3DModelling/Comparisoft/VTK/VTK-bin/Release/CaroleLowerProduction.stl";
 	//filePathProduction = "C:/Development/Capstone/Capstone3DModelling/Comparisoft/VTK/VTK-bin/Release/CaroleLowerReference.stl";
+
+	/* Create HTML file for Comparison report */
+	char report_path[100];
+	char ext[] = "/report.html";
+	sprintf(report_path, "%s%s", sloc, ext);
+	ofstream report_output;
+	report_output.open(report_path);
+
+	/* Set-up HTML file */
+	report_output << "<!DOCTYPE html>\n";
+	report_output << "<html>\n";
+	report_output << "\t<head>\n";
+	report_output << "\t\t<meta charset=\"UTF-8\">\n";
+	report_output << "\t\t<title>Comparisoft</title>\n";
+	report_output << "\t</head>\n";
+	report_output << "\t<body>\n";
+
+	/* Add all report info here */
+	report_output << "\t\t<h1>Comparison Report</h1>\n";
+	report_output << "\t\t<p>Technician: " << tname << "</p>\n";
+	report_output << "\t\t<p>Client: " << cname << "</p>\n";
+	report_output << "\t\t<p>Patient: " << pname << "</p>\n";
+
+	report_output << "\t</body>\n";
+	report_output << "</html>\n";
+
+	/* Close will occur following alignment once the algorithm is generating values */
+	report_output.close();
 
 	//Launch the VTK function
 	VTKmain(filePathReference, filePathProduction);
