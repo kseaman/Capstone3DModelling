7234798194c5296df51ca3d842067f069437e44c
Karn <karnseaman@gmail.com>
2018-02-27 22:59:57 -0800

    Fix merging issue

--- Comparisoft/VTK/VTK.h
+++ Comparisoft/VTK/VTK.h
@@ -2,8 +2,5 @@
 
 #include <string>
 
-
-//std::string utf8_encode(const std::wstring &wstr);
-
-int VTKmain(char* filePathSource, char* filePathTarget, char* filename);
-
+std::string utf8_encode(const std::wstring &wstr);
+int VTKmain(char* filePathSource, char* filePathTarget, char* filename, char *camera);
