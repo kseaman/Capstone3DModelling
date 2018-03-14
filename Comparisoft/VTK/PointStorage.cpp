523ab23e8dae8bc1a04e24a7e54e3070d53c0296
Karn <karnseaman@gmail.com>
2018-03-14 00:07:17 -0700

    worked on distance calculation

--- Comparisoft/VTK/PointStorage.cpp
+++ Comparisoft/VTK/PointStorage.cpp
@@ -1,4 +1,5 @@
 #include "PointStorage.h"
+#include <vtkCellLocator.h>
 using namespace std;
 
 
@@ -11,13 +12,71 @@ using namespace std;
 		targetList.reserve(sizeT);
 		sourceData = source;
 		targetData = target;
+
+		//cout << endl << sizeS << endl << sizeT << endl;
 	}
 
+	//run distance calculation for source file
 	void pointStorage::calculateSource() {
 
+		//set up locator
+		vtkSmartPointer<vtkCellLocator> cellLocator =
+			vtkSmartPointer<vtkCellLocator>::New();
+		cellLocator->SetDataSet(targetData);
+		cellLocator->BuildLocator();
+
+		//setup variables for locator
+		double cp[3];
+		double dist;
+		vtkIdType cellId;
+		int subId;
+
+		double p[3];
+
+		for (vtkIdType i = 0; i < sizeS; i++) {
+
+			//get the point to query
+			sourceData->GetPoint(i, p);
+
+			//calculate distance
+			cellLocator->FindClosestPoint(p, cp, cellId, subId, dist);
+
+			//insert the point and its distance into the vector
+			point point(i, dist);
+			sourceList.push_back(point);
+		}
+
 	}
 
+	//run distance calculation for target file
 	void pointStorage::calculateTarget() {
+
+		//set up locator
+		vtkSmartPointer<vtkCellLocator> cellLocator =
+			vtkSmartPointer<vtkCellLocator>::New();
+		cellLocator->SetDataSet(sourceData);
+		cellLocator->BuildLocator();
+
+		//setup variables for locator
+		double cp[3];
+		double dist;
+		vtkIdType cellId;
+		int subId;
+
+		double p[3];
+
+		for (vtkIdType i = 0; i < sizeT; i++) {
+
+			//get the point to query
+			sourceData->GetPoint(i, p);
+
+			//calculate distance
+			cellLocator->FindClosestPoint(p, cp, cellId, subId, dist);
+
+			//insert the point and its distance into the vector
+			point point(i, dist);
+			targetList.push_back(point);
+		}
 
 	}
 
